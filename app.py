bea2f8a537be0924e3ca9db4bd74956457f532e7
LuiguiSD48 <125932428+LuiguiSD48@users.noreply.github.com>
2023-06-29 17:11:30 -0600

    se agrego login y registros

--- app.py
+++ app.py
@@ -22,15 +22,25 @@ mysql=MySQL(app)
 
 #ruta index o principal http://localhost:5000
 #la ruta se compone de nombre y la funcion
+@app.route('/')
+def index():
+    return render_template('login.html')
 
+@app.route('/registrar')
+def registrar():
+    return render_template('RegistrarUsuarios.html')
+
+@app.route('/iniciars')
+def iniciars():
+    return render_template('login.html')
 
 
 #Creando mi primer Decorador o ruta para el Home
-@app.route('/', methods=['GET','POST'])
-def inicio():
+@app.route('/mostrar', methods=['GET','POST'])
+def mostrar():
     
     CS=mysql.connection.cursor()
-    CS.execute ("SELECT * FROM autos ORDER BY id_auto")
+    CS.execute ("SELECT * FROM autos")
     mysql.connection.commit()
     data = CS.fetchall() #fetchall () Obtener todos los registros
 
@@ -39,7 +49,20 @@ def inicio():
     print(total)
     return render_template('consultar.html', dataAutos = data, dataTotal = total)
 
+@app.route('/iniciar',methods=['POST'])
+def iniciar():
+    if request.method == 'POST':
+        nombre= request.form['txtnombre']
+        contraseña= request.form['txtcontra']
+        CS = mysql.connection.cursor()
+        CS.execute('select * from usuarios where usuario=(%s) and contrasena=(%s)', (nombre,contraseña))
+        if (CS.rowcount == 1):
+            return render_template('consultar.html')
+        else:
+            return render_template('Login.html')
 
+    flash('Album Agregado Correctamente bro')
+    return redirect(url_for('index'))
 
 #ejecucion 
 if __name__== '__main__':
